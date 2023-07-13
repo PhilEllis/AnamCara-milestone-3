141687216bf37d2898a4b92474cf2e8e43c24106
PhilEllis <philippamacnab@hotmail.com>
2023-07-13 11:12:33 +0000

    feat!: notes and add note created and connected

--- notemanager/routes.py
+++ notemanager/routes.py
@@ -6,3 +6,13 @@ from notemanager.models import Note
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+
+@app.route("/add_note", methods=["GET", "POST"])
+def add_note():
+    return render_template("add_note.html")
