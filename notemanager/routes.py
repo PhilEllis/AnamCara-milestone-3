863cb4fc1c7da8be909fd68d4ab53fc49e85c8fa
PhilEllis <philippamacnab@hotmail.com>
2023-07-24 21:59:48 +0000

    Fix!: Note delete func fix modal now in for loop

--- notemanager/routes.py
+++ notemanager/routes.py
@@ -136,7 +136,7 @@ def edit_note(note_id):
     return render_template("edit_note.html", note=note)
 
 
-@app.route("/delete_note/<int:note_id>")
+@app.route("/delete_note/<int:note_id>", methods=["GET", "POST"])
 def delete_note(note_id):
     """Handle deleting a note."""
     note = Note.query.get_or_404(note_id)
--- notemanager/static/js/script.js
+++ notemanager/static/js/script.js
@@ -1,9 +1,7 @@
-<script>
-  // Delete Note Button
-  var deleteNoteBtn = document.getElementById("deleteNoteBtn");
-  deleteNoteBtn.addEventListener("click", function() {
-    var noteId = document.getElementById("deleteModal").getAttribute("data-noteid");
-    var deleteUrl = "{{ url_for('delete_note', note_id='') }}" + noteId;
-    window.location.href = deleteUrl;
-  });
-</script>
+// Delete Note Button
+var deleteNoteBtn = document.getElementById("deleteNoteBtn");
+deleteNoteBtn.addEventListener("click", function() {
+  var noteId = document.getElementById("deleteModal").getAttribute("data-noteid");
+  var deleteUrl = "{{ url_for('delete_note', note_id='') }}" + noteId;
+  window.location.href = deleteUrl;
+});
